6810a6c3d395abfd5f1972ca4b5d4bcb93ad6fa9
Fredius Tout Court <frediustcDev@gmail.com>
2018-05-04 18:01:11 +0000

    files sent to client

--- client/init.js
+++ client/init.js
@@ -1,1 +1,17 @@
-import '../imports/client/main.js'
+// import '../imports/client/main.js'
+import { Meteor } from "meteor/meteor";
+import React from "react";
+import { render } from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+
+import "./src/Less/main.less";
+import App from "./src/App.js";
+
+const appRouter = (
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
+Meteor.startup(() => {
+  render(appRouter, document.getElementById("root"));
+});
